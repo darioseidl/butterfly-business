9637846bb39889b6dae87c013f57cb3a2f981f1e
Dario Seidl <darioseidl@fastmail.fm>
2019-02-16 23:14:55 +0100

    cleanup main method

--- src/e1/PaymentApplication.java
+++ src/e1/PaymentApplication.java
@@ -1,7 +1,5 @@
 package e1;
 
-import e1.credit.CreditCards;
-import e1.credit.LuhnValidator;
 import e1.csv.CSVFileWriter;
 import e1.files.FileReader;
 import e1.intelligence.BusinessFileWriter;
@@ -24,26 +22,9 @@ public class PaymentApplication {
 
         List<String> parsedLines = IntStream.iterate(0, i -> i + 3).limit(lines.size() / 3)
                 .mapToObj(i -> {
-
-
-                    List<String> partition0 = partition(lines.get(i));
-                    List<String> partition1 = partition(lines.get(i + 1));
-                    List<String> partition2 = partition(lines.get(i + 2));
-
-
-                    List<String> strangeList = IntStream.range(0, partition0.size())
-                            .mapToObj(index -> partition0.get(index) + partition1.get(index) + partition2.get(index))
-                            .collect(Collectors.toList());
-
-
-                    String digits = strangeList.stream()
-                            .map(digit -> SevenSegmentMapping.strangeDigits.get(digit))
-                            .collect(Collectors.joining());
-
-
-                    return digits;
-                }).collect(Collectors.toList());
-
+                    return translateStrangeDigits(lines, i);
+                })
+                .collect(Collectors.toList());
 
         List<PaymentOperation> paymentOperations = getPaymentOperations(parsedLines);
 
@@ -56,7 +37,21 @@ public class PaymentApplication {
         BusinessFileWriter.writeBusinessIntelligenceReport(paymentOperations, "output/Report.txt");
     }
 
-    public static List<PaymentOperation> getPaymentOperations(List<String> parts) {
+    private static String translateStrangeDigits(List<String> lines, int i) {
+        List<String> partition0 = partition(lines.get(i), 3);
+        List<String> partition1 = partition(lines.get(i + 1), 3);
+        List<String> partition2 = partition(lines.get(i + 2), 3);
+
+        List<String> strangeList = IntStream.range(0, partition0.size())
+                .mapToObj(index -> partition0.get(index) + partition1.get(index) + partition2.get(index))
+                .collect(Collectors.toList());
+
+        return strangeList.stream()
+                .map(digit -> SevenSegmentMapping.strangeDigits.get(digit))
+                .collect(Collectors.joining());
+    }
+
+    private static List<PaymentOperation> getPaymentOperations(List<String> parts) {
         return parts.stream()
                 .map(line -> line.split(" "))
                 .map(columns -> new PaymentOperation(
@@ -66,9 +61,9 @@ public class PaymentApplication {
                 .collect(Collectors.toList());
     }
 
-    static List<String> partition(String line) {
-        return IntStream.iterate(0, i -> i + 3).limit(line.length() / 3)
-                .mapToObj(index -> line.substring(index, index + 3))
+    private static List<String> partition(String line, int size) {
+        return IntStream.iterate(0, i -> i + size).limit(line.length() / size)
+                .mapToObj(index -> line.substring(index, index + size))
                 .collect(Collectors.toList());
     }
 
