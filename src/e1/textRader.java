1f4a936ab9d813d9c9dbb4a644263d06729a2b1d
Dario Seidl <darioseidl@fastmail.fm>
2019-02-16 16:55:32 +0100

    parse strange digits for all lines

--- src/e1/textRader.java
+++ src/e1/textRader.java
@@ -6,37 +6,43 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class textRader {
+
     public static void main(String[] args) {
-
-
-
 
         FileReader fileReader = new FileReader();
         List<String> lines = fileReader.asList("Files/buterfly-business.txt");
 
-        List<String> partition0 = partition(lines.get(0));
-        List<String> partition1 = partition(lines.get(1));
-        List<String> partition2 = partition(lines.get(2));
+        List<String> parsedLines = IntStream.iterate(0, i -> i + 3).limit(lines.size()/3)
+                .mapToObj(i -> {
 
-        List<String> strangeList = IntStream.range(0, partition0.size())
-                .mapToObj(index -> partition0.get(index) + partition1.get(index) + partition2.get(index))
-                .collect(Collectors.toList());
+            List<String> partition0 = partition(lines.get(i));
+            List<String> partition1 = partition(lines.get(i+1));
+            List<String> partition2 = partition(lines.get(i+2));
+
+            List<String> strangeList = IntStream.range(0, partition0.size())
+                    .mapToObj(index -> partition0.get(index) + partition1.get(index) + partition2.get(index))
+                    .collect(Collectors.toList());
 
 //        for (String s : strangeList) {
 //            System.out.println(s);
 //        }
 
-        List<String> digits = strangeList.stream()
-                .map(digit -> SevenSegmentMapping.strangeDigits.get(digit))
-                .collect(Collectors.toList());
+            String digits = strangeList.stream()
+                    .map(digit -> SevenSegmentMapping.strangeDigits.get(digit))
+                    .collect(Collectors.joining());
 
-        System.out.println(digits);
+//            System.out.println(digits);
 
+            return digits;
+
+        }).collect(Collectors.toList());
+
+//        System.out.println(parsedLines);
     }
 
     static List<String> partition(String line) {
-        return IntStream.range(0, line.length()/3)
-                .mapToObj(index -> line.substring(index*3, index*3 + 3))
+        return IntStream.iterate(0, i -> i + 3).limit(line.length()/3)
+                .mapToObj(index -> line.substring(index, index + 3))
                 .collect(Collectors.toList());
     }
 
