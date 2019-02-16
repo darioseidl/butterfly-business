fd00aec3f3fc2497b0a8f65a418d69dd2167d1d8
hamadeabdo <you@example.comcd>
2019-02-16 17:46:28 +0100

    our first ex1

--- src/e1/textRader.java
+++ src/e1/textRader.java
@@ -1,9 +1,9 @@
 package e1;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+
 
 public class textRader {
 
@@ -12,36 +12,52 @@ public class textRader {
         FileReader fileReader = new FileReader();
         List<String> lines = fileReader.asList("Files/buterfly-business.txt");
 
-        List<String> parsedLines = IntStream.iterate(0, i -> i + 3).limit(lines.size()/3)
+        List<String> parsedLines = IntStream.iterate(0, i -> i + 3).limit(lines.size() / 3)
                 .mapToObj(i -> {
 
-            List<String> partition0 = partition(lines.get(i));
-            List<String> partition1 = partition(lines.get(i+1));
-            List<String> partition2 = partition(lines.get(i+2));
+                    List<String> partition0 = partition(lines.get(i));
+                    List<String> partition1 = partition(lines.get(i + 1));
+                    List<String> partition2 = partition(lines.get(i + 2));
 
-            List<String> strangeList = IntStream.range(0, partition0.size())
-                    .mapToObj(index -> partition0.get(index) + partition1.get(index) + partition2.get(index))
-                    .collect(Collectors.toList());
+                    List<String> strangeList = IntStream.range(0, partition0.size())
+                            .mapToObj(index -> partition0.get(index) + partition1.get(index) + partition2.get(index))
+                            .collect(Collectors.toList());
 
 //        for (String s : strangeList) {
 //            System.out.println(s);
 //        }
 
-            String digits = strangeList.stream()
-                    .map(digit -> SevenSegmentMapping.strangeDigits.get(digit))
-                    .collect(Collectors.joining());
+                    String digits = strangeList.stream()
+                            .map(digit -> SevenSegmentMapping.strangeDigits.get(digit))
+                            .collect(Collectors.joining());
 
-//            System.out.println(digits);
+            System.out.println(digits);
 
-            return digits;
+                    return digits;
 
-        }).collect(Collectors.toList());
+                }).collect(Collectors.toList());
 
 //        System.out.println(parsedLines);
+        List<document> documents = getDocument(parsedLines);
+        System.out.println(documents);
+
+    }
+
+    public static List<document> getDocument(List<String> parts) {
+        return parts.stream()
+                .map(line -> line.split(" "))
+                .map(columns -> new document(
+                        columns[0],
+                        columns[1],
+                        columns[2]))
+
+                .collect(Collectors.toList());
+
+
     }
 
     static List<String> partition(String line) {
-        return IntStream.iterate(0, i -> i + 3).limit(line.length()/3)
+        return IntStream.iterate(0, i -> i + 3).limit(line.length() / 3)
                 .mapToObj(index -> line.substring(index, index + 3))
                 .collect(Collectors.toList());
     }
