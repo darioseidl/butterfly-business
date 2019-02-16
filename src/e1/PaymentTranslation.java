ba5365bb797af26648a1a0c5e312e74bf77c1762
hamadeabdo <you@example.comcd>
2019-02-16 22:00:36 +0100

    clean up

--- src/e1/PaymentTranslation.java
+++ src/e1/PaymentTranslation.java
@@ -15,51 +15,40 @@ public class PaymentTranslation {
     public static void main(String[] args) {
 
         FileReader fileReader = new FileReader();
-        List<String> lines = fileReader.asList("Files/buterfly-business.txt");
 
+        List<String> lines = fileReader.asList("Files/buterfly-business.txt");
         List<String> parsedLines = IntStream.iterate(0, i -> i + 3).limit(lines.size() / 3)
                 .mapToObj(i -> {
+
 
                     List<String> partition0 = partition(lines.get(i));
                     List<String> partition1 = partition(lines.get(i + 1));
                     List<String> partition2 = partition(lines.get(i + 2));
 
+
                     List<String> strangeList = IntStream.range(0, partition0.size())
                             .mapToObj(index -> partition0.get(index) + partition1.get(index) + partition2.get(index))
                             .collect(Collectors.toList());
 
-//        for (String s : strangeList) {
-//            System.out.println(s);
-//        }
 
                     String digits = strangeList.stream()
                             .map(digit -> SevenSegmentMapping.strangeDigits.get(digit))
                             .collect(Collectors.joining());
 
-//            System.out.println(digits);
 
                     return digits;
-
                 }).collect(Collectors.toList());
 
-//        System.out.println(parsedLines);
+
         List<PaymentOperation> documents = getDocument(parsedLines);
 
-//        System.out.println(documents);
-//        LuhnValidator luhnValidator = new LuhnValidator();
-//        List<String> collect = documents.stream()
-//                .map(e -> getIssuer(e.getCriditNumb()))
-//                .collect(Collectors.toList());
-//        System.out.println(collect);
 
         documents.forEach(e -> e.setIssuer(getIssuer(e.getCriditNumb())));
 
         documents.sort(Comparator.comparing(PaymentOperation::getDate));
 
-//        String collect = documents.stream().map(e -> e.getAmount().toString() + "+").collect(Collectors.joining());
-//        System.out.println(collect);
 
-        List<String> csvLines = documents.stream().map(e -> e.toCSV()).collect(Collectors.toList());
+        List<String> csvLines = documents.stream().map(PaymentOperation::toCSV).collect(Collectors.toList());
 
         System.out.println(String.join("\n", csvLines));
 
@@ -72,9 +61,6 @@ public class PaymentTranslation {
         System.out.println("The total is " + totalAmount);
 
 
-//        Map<YearMonth, List<document>> groupByYearMonth = documents.stream()
-//                .collect(Collectors.groupingBy(document::getYearMonth));
-//        System.out.println(groupByYearMonth);
 
         Map<YearMonth, Double> groupSumByYearMonth = documents.stream()
                 .collect(Collectors.groupingBy(PaymentOperation::getYearMonth, Collectors.summingDouble(PaymentOperation::getAmount)));
@@ -83,15 +69,11 @@ public class PaymentTranslation {
 
         Map.Entry<YearMonth, Double> mostProfitable =
                 groupSumByYearMonth.entrySet().stream()
-                        .max((entry1, entry2) -> Double.compare(entry1.getValue(), entry2.getValue()))
+                        .max(Comparator.comparingDouble(Map.Entry::getValue))
                         .get();
 
         System.out.println("most profitable month: " + mostProfitable.getKey().getMonth().getDisplayName(TextStyle.FULL, Locale.ENGLISH));
 
-//        Map<YearMonth, Double> groupAverageByYearMonth = documents.stream()
-//                .collect(Collectors.groupingBy(document::getYearMonth, Collectors.averagingDouble(document::getAmount)));
-//
-//        System.out.println("average by month: " + groupAverageByYearMonth);
 
         Map<String, Long> issuerCount = documents.stream()
                 .collect(Collectors.groupingBy(PaymentOperation::getIssuer, Collectors.counting()));
@@ -100,7 +82,7 @@ public class PaymentTranslation {
 
         Map.Entry<String, Long> mostUsedIssuer =
                 issuerCount.entrySet().stream()
-                        .max((entry1, entry2) -> Long.compare(entry1.getValue(), entry2.getValue()))
+                        .max(Comparator.comparingLong(Map.Entry::getValue))
                         .get();
 
         System.out.println("most used issuer: " + mostUsedIssuer);
@@ -128,6 +110,15 @@ public class PaymentTranslation {
         else
             return issuer;
     }
+
+
+
+
+
+
+
+
+
 
     public static List<PaymentOperation> getDocument(List<String> parts) {
         return parts.stream()
